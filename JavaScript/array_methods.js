1133af06eb052dfbbe611d287eb4b64194740515
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-09-19 21:24:44 +0700

    Lession 1: overview javascript

---
+++ JavaScript/array_methods.js
@@ -0,0 +1,20 @@
+// duyệt mảng
+let jobs = ["java", "react", "angular", "vuejs"]
+// for (let i = 0; i < jobs.length; i++) {
+//     console.log(jobs[i]);
+// }
+
+// for..in
+// for(let index in jobs){
+//     console.log(jobs[index], index);
+// }
+
+// for..of
+// for(let job of jobs){
+//     console.log(job, jobs.indexOf(job));
+// }
+
+// forEach
+// jobs.forEach(function(value, index){
+//     console.log(value, index);
+// })
