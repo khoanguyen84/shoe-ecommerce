d38c97f0f46ca88be04d08101cc65731e17f303a
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-09-26 20:42:44 +0700

    oop

---
+++ ES6/set.js
@@ -0,0 +1,19 @@
+// Set vs Array
+// unique (duy nhất)
+// ko index
+
+let animals = new Set(["Gà", "Vịt"])
+animals.add("Bò")
+animals.add("Lợn")
+console.log(animals.values());
+// for(let animal of animals.values()){
+//     console.log(animal);
+// }
+
+animals.delete("Bò")
+// console.log(animals.values());
+animals.forEach(function(animal) {
+    console.log(animal);
+})
+animals.clear()
+console.log(animals.values());
