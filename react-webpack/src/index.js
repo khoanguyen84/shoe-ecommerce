aafac91d37448c74435e13e947026e96bb29870e
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-10-10 20:01:45 +0700

    webpack

--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import flower from '../src/assets/flower.jpg'
 
 function App() {
     return (
         <div>
-            <h1 className="heading_1">React Webpack</h1>
+            <h1 className="heading_1" style={{color: "red", backgroundColor: "yellow"}}>React Webpack</h1>
             <a href="#">Home Page</a>
             <h3>React JS</h3>
+            <img className="img-md" src={flower} alt="" />
         </div>
     )
 }
