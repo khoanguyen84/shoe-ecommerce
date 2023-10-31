9eb692cc7e7851f2631c4cab15297c0fc59fe7af
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-10-31 21:28:28 +0700

    tranfer data throw props

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
+import { useState } from 'react';
+import Panel from './components/Panel';
+
+// App -> Panel -> LoginForm
+// state theme
+// theme -> Panel
+//            => them -> Login
+
+
+// context API
+// 1. createContext
+// 2. provider
+// 3. consumer
 
 function App() {
+  const [theme, setTheme] = useState('light')
+  const handleChangeTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
   return (
     <div className="container">
-      
+      {/* <button className='btn btn-sm btn-danger'
+        onClick={handleChangeTheme}
+      >Dark Mode</button> */}
+      <Panel theme={theme} handleChangeTheme={handleChangeTheme} />
     </div>
   );
 }
