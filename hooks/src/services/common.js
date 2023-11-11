2e8a4fbdaf52eb897c95074dc98c5ca3552cb18f
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-11-11 21:34:52 +0700

    change api urls

--- hooks/src/services/common.js
+++ hooks/src/services/common.js
@@ -1,2 +1,6 @@
-export const DEPARTMENT_API_URL = 'https://6543a6a201b5e279de20ba5b.mockapi.io/department'
-export const TEACHER_API_URL = 'https://6543a6a201b5e279de20ba5b.mockapi.io/teacher'
+export const DEPARTMENT_API_URL = 'https://json-server-restful-api.vercel.app/api/department'
+// 'http://localhost:3000/department'
+//'https://6543a6a201b5e279de20ba5b.mockapi.io/department'
+export const TEACHER_API_URL = 'https://json-server-restful-api.vercel.app/api/teacher'
+//'http://localhost:3000/teacher'
+//'https://6543a6a201b5e279de20ba5b.mockapi.io/teacher'
