14007880649aaba1b5ec919da4b086bf6d324527
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-11-14 20:57:57 +0700

    useReducer

--- hooks/src/App.js
+++ hooks/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import BankingApp from './components/BankingApp';
+import TodoApp from './components/TodoApp';
 
 function App() {
   return (
     <div className="container">
-      
+        <BankingApp/>
     </div>
   );
 }
---
+++ hooks/src/reducer/action.js
@@ -0,0 +1,18 @@
+// 2. action là 1 object
+//    action creator là 1 hàm => return object
+export const inputAmount = (payload) => {
+    return {
+        type: 'bank/inputamount',
+        payload: payload
+    }
+}
+export const deposit = () => {
+    return {
+        type: 'bank/deposit'
+    }
+}
+export const withDraw = () => {
+    return {
+        type: 'bank/withdraw'
+    }
+}
