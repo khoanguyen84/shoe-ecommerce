d6e6c2048d1288ee34ae3b5f2d43e16e7a92f598
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-11-17 14:54:34 +0700

    update

--- ecommerce-redux/src/components/Sidebar/Sidebar.jsx
+++ ecommerce-redux/src/components/Sidebar/Sidebar.jsx
@@ -5,7 +5,7 @@ import Colors from "./Colors";
 import { FaCartPlus } from "react-icons/fa";
 function Sidebar() {
     return (
-        <div className="d-flex flex-column border-end me-1 vh-100 w-25">
+        <div className="d-flex flex-column border-end me-1 h-100 w-25">
             <div className="py-3 d-flex align-items-center">
                 <FaCartPlus size={25} className="me-2" /> Shoe Ecommerce
             </div>
