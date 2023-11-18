0c988ab97993d2086b35b3851dc05739bad921a2
KhoaNguyen <nguyenhuuanhkhoa@gmail.com>
2023-11-18 22:33:33 +0700

    advance filtering using context api and useReducer

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import ShoeProvider from './context/ShoeContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ShoeProvider>
+      <App />
+    </ShoeProvider>
   </React.StrictMode>
 );
-
